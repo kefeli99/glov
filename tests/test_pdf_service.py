2250555e538560dcb782021c8ac7769eff72c816
Dogukan Kefeli <dogukankefeli@protonmail.com>
2024-08-15 13:56:56 +0300

    style(test_pdf_service.py): add return type annotation '-> None' to test_download_pdf and test_validate_pdf_url

--- tests/test_pdf_service.py
+++ tests/test_pdf_service.py
@@ -3,10 +3,10 @@ from glov.pdf_utils import PDFService
 PDF_URL = "https://www.fia.com/sites/default/files/fia_2024_formula_1_technical_regulations_-_issue_1_-_2023-04-25.pdf"
 
 
-def test_download_pdf():
+def test_download_pdf() -> None:
     pdf_path = PDFService.download_pdf(PDF_URL)
     assert pdf_path.endswith(".pdf")
 
 
-def test_validate_pdf_url():
+def test_validate_pdf_url() -> None:
     assert PDFService.validate_pdf_url(PDF_URL) is True
